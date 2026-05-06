d852daf65c880e4e3d6571ba3ffdc067b70c7bdc
fabricio-dev-front <fabriciopereiradasilva477@gmail.com>
2026-05-06 09:16:10 -0300

    fix: adjustments

--- components/CardProduto/index.tsx
+++ components/CardProduto/index.tsx
@@ -7,7 +7,7 @@ export function CardProduto({
 }: Produto & { className?: string }) {
   return (
     <div
-      className={`${className} border border-white/5 px-3 py-1 rounded-md m-1.5 cursor-pointer flex items-center gap-3 justify-between`}
+      className={`${className} border border-black px-3 py-1 rounded-md m-1.5 cursor-pointer flex items-center gap-3 justify-between`}
     >
       <p className="font-normal text-base">{nome}</p>
       <p className="font-normal text-green-500 text-base">R$ {preco}</p>
